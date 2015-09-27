abdafbadad98fb5d93fdc26ab02ac6e91f65e025
AJ Cave <acave@cj.com>
2015-09-27 01:15:52 -0700

    Added Dockerfile and updated Main to use Docker config if necessary

---
+++ Dockerfile
@@ -0,0 +1,40 @@
+FROM ubuntu:14.04
+
+# Install Java 8
+RUN echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | debconf-set-selections 
+RUN apt-get install -y software-properties-common git wget
+RUN add-apt-repository -y ppa:webupd8team/java
+RUN apt-get update
+RUN apt-get install -y oracle-java8-installer
+RUN rm -rf /var/lib/apt/lists/*
+RUN rm -rf /var/cache/oracle-jdk8-installer
+
+# Define commonly used JAVA_HOME variable
+ENV JAVA_HOME /usr/lib/jvm/java-8-oracle
+
+#install maven
+RUN /bin/bash -c -l "wget http://apache.arvixe.com/maven/maven-3/3.2.5/binaries/apache-maven-3.2.5-bin.tar.gz"
+
+RUN /bin/bash -c -l "tar -zxf apache-maven-3.2.5-bin.tar.gz"
+
+RUN /bin/bash -c -l "cp -R apache-maven-3.2.5 /usr/local"
+
+RUN /bin/bash -c -l "ln -s /usr/local/apache-maven-3.2.5/bin/mvn /usr/bin/mvn"
+
+#pull project from GIT
+RUN mkdir -p /app/todo-react-persistence
+#ADD . /app/todo-react-persistence
+RUN /bin/bash -l -c "git clone https://github.com/cavenator/todo-react-persistence.git /app/todo-react-persistence"
+
+WORKDIR /app/todo-react-persistence
+
+#package the one-jar
+RUN /bin/bash -c -l "mvn package -DskipTests"
+
+#expose the port
+EXPOSE 8080
+
+ENTRYPOINT ["java","-jar","target/todo-app-persistence-0.1-SNAPSHOT.one-jar.jar","true"]
+
+#NOTE: If you are going to run the app from a docker container, you must edit the hibernate config so you could link up the DB from within the container
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.hibernate.cfg.Configuration;
 import org.httpobjects.HttpObject;
 import org.httpobjects.Request;
 import org.httpobjects.Response;
@@ -15,8 +16,27 @@ import java.util.Map;
 
 public class Main {
 
+   private static Configuration getDockerDBConfiguration(){
+       String dbAddress = System.getenv("DB_PORT_5432_TCP_ADDR");
+       String dbPort = System.getenv("DB_PORT_5432_TCP_PORT");
+       String dbUrl = "jdbc:postgresql://"+dbAddress+":"+dbPort+"/postgres";
+       Configuration configuration = new Configuration();
+       configuration.setProperty("hibernate.connection.driver_class","org.postgresql.Driver");
+       configuration.setProperty("hibernate.connection.url",dbUrl);
+       configuration.setProperty("hibernate.connection.username","postgres");
+       configuration.setProperty("hibernate.dialect","org.hibernate.dialect.PostgreSQLDialect");
+       configuration.addAnnotatedClass(org.example.Todo.class);
+       return configuration;
+   }
+
    public static void main(String[] args) throws Exception {
-       TodoDao todoDao = new TodoDao();
+       TodoDao todoDao;
+       if (args.length > 0 && Boolean.valueOf(args[0])){
+          todoDao = new TodoDao(getDockerDBConfiguration());
+       } else {
+           todoDao = new TodoDao();
+       }
+
        final String RESOURCES_PATH = "/";
 
        ClasspathResourceObject rootResource = new ClasspathResourceObject(RESOURCES_PATH,"/index.html", Main.class);
