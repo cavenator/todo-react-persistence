c23c172521d5991508da1432344d46b363e45b9d
AJ Cave <acave@cj.com>
2015-10-13 18:03:34 -0700

    Updated start-todo-db.sh to use init-todo-db.sql to initialize db

--- start-todo-db.sh
+++ start-todo-db.sh
@@ -9,13 +9,10 @@ if [ $count -eq 1 ]
         echo "Note to self .... create data volume for this container and learn how to back it up in case of container failure"
    else
        echo "okay .... Lets make a new one!"
-       /bin/bash ./create-db-container.sh
        sudo docker run -d --name=todo-db -p 127.0.0.1:5432:5432 postgres:9.1.18
        echo "Docker container running postgres db up and going .... going to wait 3 seconds before setting up db"
        sleep 3
        echo "creating todo table...."
-       psql -h localhost -p 5432 -U postgres -c "create table todo(id integer, title varchar(40), description varchar(200));"
-       echo "creating sequence for table ...."
-       psql -h localhost -p 5432 -U postgres -c "create sequence todo_seq start 1;"
+       psql -h localhost -p 5432 -U postgres < init_todo_db.sql
        echo "Done! Ready to rock and roll!"
 fi
